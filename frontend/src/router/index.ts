6e78e5228783d28f2bc56310c4c2099bdd7ad6fb
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2025-04-12 12:05:40 -0300

    importando rotas

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+import TaskList from '../components/TaskList.vue';
+import TaskDetail from '../components/TaskDetail.vue';
+import UserManagement from '../components/UserManagement.vue';
+import ProjectManagement from '../components/ProjectManagement.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', name: 'TaskList', component: TaskList },
+  { path: '/task/:id', name: 'TaskDetail', component: TaskDetail, props: true },
+  { path: '/users', name: 'UserManagement', component: UserManagement },
+  { path: '/projects', name: 'ProjectManagement', component: ProjectManagement },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
+
